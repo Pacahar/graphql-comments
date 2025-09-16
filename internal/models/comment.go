cf79879827e78730725c22cba4cfc2a9f78d4d0d
Pacahar <s.kirill29@gmail.com>
2025-09-16 16:36:23 +0300

    Create models

---
+++ internal/models/comment.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type Comment struct {
+	ID        int64     `json:"id"`
+	PostID    int64     `json:"post_id"`
+	ParentID  *int64    `json:"parent_id,omitempty"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+}
