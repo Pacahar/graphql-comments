e1a29337343938456baae7a1861e32d7fdc01167
Pacahar <s.kirill29@gmail.com>
2025-09-16 22:21:46 +0300

    add CommentsDisabled field to Post model

--- internal/models/post.go
+++ internal/models/post.go
@@ -3,8 +3,9 @@ package models
 import "time"
 
 type Post struct {
-	ID        int64     `json:"id"`
-	Title     string    `json:"title"`
-	Content   string    `json:"content"`
-	CreatedAt time.Time `json:"created_at"`
+	ID               int64     `json:"id"`
+	Title            string    `json:"title"`
+	Content          string    `json:"content"`
+	CommentsDisabled bool      `json:"comments_disabled"`
+	CreatedAt        time.Time `json:"created_at"`
 }
