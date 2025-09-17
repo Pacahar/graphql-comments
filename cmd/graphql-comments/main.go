1fa81e40926df54be04e442fb2a75483a32dfcaa
Pacahar <s.kirill29@gmail.com>
2025-09-17 20:02:20 +0300

    add storage initialization to main

--- cmd/graphql-comments/main.go
+++ cmd/graphql-comments/main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
 
 	"github.com/Pacahar/graphql-comments/internal/config"
-)
-
-const (
-	envLocal string = "local"
-	envDev   string = "dev"
-	envProd  string = "prod"
+	"github.com/Pacahar/graphql-comments/internal/constants"
+	"github.com/Pacahar/graphql-comments/internal/storage"
+	"github.com/Pacahar/graphql-comments/internal/storage/memory"
+	"github.com/Pacahar/graphql-comments/internal/storage/postgres"
 )
 
 func main() {
@@ -22,24 +21,48 @@ func main() {
 	log.Info("Starting service", slog.String("env", cfg.Environment))
 	log.Debug("Debug messages enabled")
 
+	storage, err := setupStorage(&cfg.Storage)
+
+	if err != nil {
+		log.Error("failed to setup storage", slog.Any("error", err))
+		os.Exit(1)
+	}
+
+	log.Info("storage set", slog.String("storage type", cfg.Storage.Type))
+
+	// TODO: ALL THE
 }
 
 func setupLogger(env string) *slog.Logger {
 	var log *slog.Logger
 
 	switch env {
-	case envLocal:
+	case constants.EnvLocal:
 		log = slog.New(
 			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
 		)
-	case envDev:
+	case constants.EnvDev:
 		log = slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
 		)
-	case envProd:
+	case constants.EnvProd:
 		log = slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
 		)
+	default:
+		return slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 	}
+
 	return log
+}
+
+func setupStorage(storageCfg *config.Storage) (*storage.Storage, error) {
+	switch storageCfg.Type {
+	case constants.StorageMemory:
+		return memory.NewMemoryStorage()
+	case constants.StoragePostgres:
+		return postgres.NewPostgresStorage(storageCfg.Postgres.DSN())
+	default:
+		return nil, fmt.Errorf("unknown storage type: %s", storageCfg.Type)
+	}
 }
---
+++ internal/constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	EnvLocal string = "local"
+	EnvDev   string = "dev"
+	EnvProd  string = "prod"
+
+	StorageMemory   string = "memory"
+	StoragePostgres string = "postgres"
+)
